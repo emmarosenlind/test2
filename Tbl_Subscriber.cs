7e5e32b37ec7fead92deb6afd8a3c7b99fe49bd6
emmarosenlind <emma.rosenlind@gmail.com>
2022-05-04 12:14:39 +0200

    Add files via upload

---
+++ Tbl_Subscriber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab2_web.Models
+{
+    public class Tbl_Subscriber
+    {
+        [Key]
+        public int Su_Id { get; set; }
+
+        public int Su_Securitynr { get; set; }
+
+        public string Su_Firstname { get; set; }
+
+        public string Su_Surname { get; set; }
+
+        public string Su_Address { get; set; }
+
+        public int Su_Postalcode { get; set; }
+    }
+}
